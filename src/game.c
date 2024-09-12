e5ead6814356a49a002333717d1284a322f6bc67
Zain <sonicfanjackson@gmail.com>
2024-09-12 10:02:34 -0400

    Built and changed dino's colour

--- src/game.c
+++ src/game.c
@@ -113,13 +113,16 @@ int main(int argc,char *argv[])
                 gf3d_model_draw(
                     dino,
                     dinoMat,
-                    GFC_COLOR_WHITE,
+                    GFC_COLOR_RED,
                     0);
                 draw_origin();
             //2D draws
                 gf2d_mouse_draw();
                 gf2d_font_draw_line_tag("ALT+F4 to exit",FT_H1,GFC_COLOR_WHITE, gfc_vector2d(10,10));
+
         gf3d_vgraphics_render_end();
+
+
         if (gfc_input_command_down("exit"))_done = 1; // exit condition
         game_frame_delay();
     }    
