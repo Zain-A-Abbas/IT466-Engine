ce9182d73cf52f314a483c5de51d1d0ff5c03ebc
Zain <sonicfanjackson@gmail.com>
2024-09-24 19:52:58 -0400

    Mouse

--- src/game.c
+++ src/game.c
@@ -99,6 +99,7 @@ int main(int argc,char *argv[])
     // Create player
     Entity * player = createPlayer();
     assignCamera(player, gf3dGetCamera());
+    SDL_SetRelativeMouseMode(SDL_TRUE);
 
 
     //windows
