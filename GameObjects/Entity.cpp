53854bf5b308e3d1a16d9ee4198a99511a8a6e0d
Zain <sonicfanjackson@gmail.com>
2024-10-09 10:14:02 -0400

    Fixed entities

---
+++ GameObjects/Entity.cpp
@@ -0,0 +1,135 @@
+#include "simple_logger.h"
+#include "gfc_matrix.h"
+#include "Entity.hpp"
+
+typedef struct {
+    Entity *entityList;
+    Uint32 entityMax;
+} EntityManager;
+
+static EntityManager entityManager = {0};
+
+Entity * entityNew() {
+    for (int i = 0; i < entityManager.entityMax; ++i) {
+        if (entityManager.entityList[i]._in_use) continue; // Skil ones in use
+        memset(&entityManager.entityList[i], 0, sizeof(Entity)); // Clear out memory if something is there
+        entityManager.entityList[i].scale = gfc_vector3d(1, 1, 1);
+        entityManager.entityList[i]._in_use = true;
+        return &entityManager.entityList[i];
+    }
+    return NULL;
+}
+
+void entitySystemInit(Uint32 maxEnts) {
+    if (entityManager.entityList) {
+        slog("Entity Manager exists.");
+        return;
+    }
+    if (maxEnts == 0) {
+        slog("Cannot allocate 0 entities.");
+        return;
+    }
+
+    entityManager.entityList = (Entity*)gfc_allocate_array(sizeof(Entity), maxEnts);
+    if (!entityManager.entityList) {
+        slog("Failed to allocate %i entities for entity manager", maxEnts);
+        return;
+    }
+    entityManager.entityMax = maxEnts;
+    atexit(entitySystemClose);
+}
+
+void entitySystemClose() {
+    for (int i = 0; i < entityManager.entityMax; ++i) {
+        if (!entityManager.entityList[i]._in_use) continue;
+        _entityFree(&entityManager.entityList[i]);
+    }
+    memset(&entityManager, 0, sizeof(EntityManager));
+}
+
+void _entityDraw(Entity * self) {
+    if (!self) return;
+    
+    if (self->draw) {
+        self->draw(self);
+        return;
+    }
+
+    GFC_Matrix4 matrix;
+    gfc_matrix4_from_vectors(
+        matrix,
+        self->position,
+        self->rotation,
+        self->scale
+    );
+
+    /*Light light = {
+        gfc_vector4d(0, 0, 0, 0),
+        gfc_vector4d(0, 0.5, -1, 0),
+        gfc_vector4d(1, 1, 1, 0),
+        0.0,
+        1.0,
+        0.2,
+        1.0,
+        1.0
+    };
+
+    GFC_List *lightList = gfc_list_new();
+    gfc_list_insert(lightList, &light, 0);
+
+    int listCount = gfc_list_count(lightList);*/
+
+    gf3d_model_draw(
+        self->model,
+        matrix,
+        GFC_COLOR_WHITE,
+        NULL,
+        0
+    );
+}
+
+void _entityThink(Entity * self) {
+    if (!self) return;
+    
+    if (self->think) {
+        self->think(self);
+        return;
+    }
+}
+
+
+void _entityUpdate(Entity * self) {
+    if (!self) return;
+    
+    if (self->update) {
+        self->update(self);
+        return;
+    }
+}
+
+void entityDrawAll() {
+    for (int i = 0; i < entityManager.entityMax; ++i) {
+        if (!entityManager.entityList[i]._in_use) continue;
+        _entityDraw(&entityManager.entityList[i]);
+    }
+}
+
+void entityUpdateAll() {
+    for (int i = 0; i < entityManager.entityMax; ++i) {
+        if (!entityManager.entityList[i]._in_use) continue;
+        _entityUpdate(&entityManager.entityList[i]);
+    }
+}
+
+void entityThinkAll() {
+    for (int i = 0; i < entityManager.entityMax; ++i) {
+        if (!entityManager.entityList[i]._in_use) continue;
+        _entityThink(&entityManager.entityList[i]);
+    }
+}
+
+void _entityFree(Entity *self) {
+    if (!self) return;
+    gf3d_model_free(self->model);
+    memset(self, 0, sizeof(Entity));
+}
---
+++ include/Entity.hpp
@@ -0,0 +1,62 @@
+#ifndef __ENTITY_SYSTEM__
+#define __ENTITY_SYSTEM__
+
+#include "gfc_types.h"
+#include "gfc_text.h"
+#include "gfc_vector.h"
+#include "gf3d_model.h"
+
+
+typedef struct Entity_S {
+    Uint8           _in_use; // Active memory
+    GFC_TextLine    name; // Entity Name
+    GFC_Vector3D    position; 
+    GFC_Vector3D    rotation;
+    GFC_Vector3D    scale;
+    Model           *model; // POINTER to model
+    // Behavior
+    void (*think)   (struct Entity_S *self); // Called every frame on the entity
+    void (*update)   (struct Entity_S *self); // Called every frame for entity state update
+    void (*draw)    (struct Entity_S *self); // Custom draw code
+    void (*free)    (struct Entity_S *self); // Cleans up custom data
+    void            *data; // Custom entity data
+
+} Entity;
+
+/**
+ * @brief Allocates blank entity for use
+ * @return NULL on failure (no memory) | POINTER to entity
+ */
+Entity * entityNew();
+
+void entitySystemClose();
+
+/**
+ * @brief Initialize entity manager
+ * @param maxEnts Man number of entities
+ */
+void entitySystemInit(Uint32 maxEnts);
+
+/**
+ * @brief Draw all the entities to screen
+ */
+void entityDrawAll();
+
+
+/**
+ * @brief Update all entities
+ */
+void entityUpdateAll();
+
+/**
+ * @brief Have all entities think
+ */
+void entityThinkAll();
+
+/**
+ * @brief Return memory of previously allocated entity back to pool
+ * @param self POINTER to entity
+ */
+void _entityFree(Entity *self);
+
+#endif
